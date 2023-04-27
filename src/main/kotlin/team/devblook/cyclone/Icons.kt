02cf71751c007cac4dbe2606cfb4d4fa4fda8946
Call4han <ccasarider@gmail.com>
2023-04-26 19:29:14 -0500

    refactor: test

--- src/main/kotlin/team/devblook/cyclone/Icons.kt
+++ src/main/kotlin/team/devblook/cyclone/Icons.kt
@@ -10,5 +10,6 @@ class DuckIcon {
     companion object {
         val RUN: ImageIcon = ImageIcon(DuckIcon::class.java.getResource("/mario.gif"))
         val BACK: ImageIcon = ImageIcon(DuckIcon::class.java.getResource("/shell.gif"))
+        //
     }
 }
